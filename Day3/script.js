fbea8850d7034044649580bd1270cec0202e1d83
sarrthak008 <sarthaknavale08@gmail.com>
2025-04-04 10:31:49 -0400

    add hamne padh data ke types

---
+++ Day3/script.js
@@ -0,0 +1,56 @@
+
+// Number 
+
+let PI = 3.14
+let pinCode = 412802 
+let mobileNumber = 8459360294 
+let roll_Number = 3468
+let age  = 20
+let radius = 7 
+let OTP = 1234
+
+//String 
+
+let email_id = "sarthaknavlae40@gmail.com"
+let college_name = "ATC BHOR"
+let address = "AT POST MORVE "
+let city_name = "rajdhani satara"
+
+
+// boolean 
+// true || false
+
+let LOGINED = true
+let USER_HAVE_ACCESS = false
+let BAN = false
+
+
+//null 
+let a = null 
+
+// undefined
+
+let b 
+
+
+
+//Non-primitive 
+
+// {}  [] () 
+
+// let obj = {}
+
+// let arr =[]
+
+// let sum(){}
+
+// test 
+
+let greet = ["hello","good","morning"]
+
+function sum(params) {
+    
+}
+
+
+
