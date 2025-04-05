24b4d8c58eb3be57f8ad1f1ad0e39e13849a70ba
sarrthak008 <sarthaknavale08@gmail.com>
2025-04-05 10:14:32 -0400

    learn about console functions

---
+++ Day4/script.js
@@ -0,0 +1,19 @@
+
+let message = "Hello World"
+
+console.log(message)
+
+let err_message = "Something went wrong"
+
+console.error(err_message)
+
+let warn_message = "faild to fetch"
+
+console.warn(warn_message)
+
+let info = "learn js with easyCode "
+
+console.info(info)
+
+// console.clear()
+
