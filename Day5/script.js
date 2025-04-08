fff97e316e5a7e83d3a787b6fb89042481314fcd
sarrthak008 <sarthaknavale08@gmail.com>
2025-04-08 10:16:40 -0400

    comparisio operators

--- Day5/script.js
+++ Day5/script.js
@@ -30,11 +30,34 @@ let e = 12
 
 e %= 2
 
-console.log(e)
+//console.log(e)
 
 
+let p = 12 
+let q = 12
+
+//console.log(p != q)
 
 
+let OTP = 1234
+
+let USER_OTP = "1234"
+
+//console.log(USER_OTP === OTP )
+
+let axy = 10 ;
+
+let byz = 20;
+
+let result =  10 < 20;
+
+//console.log(result)
 
 
+let res = 12 >= 12
 
+//console.log(res)
+
+let res1 =  12 <= 12
+
+console.log(res1)
