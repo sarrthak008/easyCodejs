46d797f328a6f4b2c724a90c0afb1b3973612e3b
sarrthak008 <sarthaknavale08@gmail.com>
2025-04-08 09:23:29 -0400

    learn about assignment operators

---
+++ Day5/script.js
@@ -0,0 +1,40 @@
+let name = "easyCode"
+
+//console.log(name)
+ 
+let a = 10
+
+a += 3
+
+//console.log(a)
+
+let b = 20 
+
+b -= 10
+
+//console.log(b)
+
+let c = 4
+
+c *= c
+
+//console.log(c)
+
+let d = 12
+
+d /= 2
+
+//console.log(d)
+
+let e = 12 
+
+e %= 2
+
+console.log(e)
+
+
+
+
+
+
+
