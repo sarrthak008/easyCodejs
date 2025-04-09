eb6990f7f9ee456e3a4630e1deab5566bd1d484a
sarrthak008 <sarthaknavale08@gmail.com>
2025-04-09 10:33:39 -0400

    learn about tarnery operator

---
+++ Day6/script.js
@@ -0,0 +1,32 @@
+
+// let age = 10;
+
+//  let result = age >= 18 ? "able to vote" : "not able to vote" ;
+
+//  console.log(result)
+
+
+// let BAN_VAlUE = true 
+
+// let res = BAN_VAlUE == true ? "you are ban" : "you are not ban"
+
+// console.log(res )
+
+
+// let myNumber = 0
+
+// let result = myNumber > 0 ? "positive" : "negative"
+
+// console.log(result)
+
+
+// let num = 10
+
+// num % 3 == 0 ? "devided by 3 " : "not devided by three"
+
+
+let num1 = 111
+
+let result = num1 % 2 == 0 ? "even" : "odd"
+
+console.log(result)
