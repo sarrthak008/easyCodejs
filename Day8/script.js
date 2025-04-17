aa0d4b64f13301072a3b74e7083d0618a5c04094
sarrthak008 <sarthaknavale08@gmail.com>
2025-04-17 10:39:47 -0400

    learn about prompot boxes in javascript

---
+++ Day8/script.js
@@ -0,0 +1,36 @@
+// alert("you are your network !");
+
+// let result = confirm("are you sure ?");
+
+// console.log(result)
+ 
+// let result = prompt("enter your name");
+
+// document.writeln("<h1> hello " + result + " welcome in our website </h1>" );
+
+
+// let name = prompt("enter your age");
+
+// console.log(typeof name);
+
+// let ope1 = prompt("enter first value : ")
+// let ope2 = prompt("enter sec value : ")
+
+
+// let numberOPE1 = parseInt(ope1)
+// let numberOPE2 = parseInt(ope2)
+
+// console.log("addition : " + (numberOPE1 + numberOPE2))
+// console.log("subtraction : " + (numberOPE1 - numberOPE2))
+// console.log("multiplication : " + (numberOPE1 * numberOPE2))
+// console.log("division : " + (numberOPE1 / numberOPE2))
+// console.log("modulus : " + (numberOPE1 % numberOPE2))
+
+
+
+
+
+// let a = "hello "
+// let b = " good night "
+
+// console.log(a + b)
