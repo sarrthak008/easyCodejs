484080229463bb3020eb9850c69f5a69cba6b4d5
sarrthak008 <sarthaknavale08@gmail.com>
2025-04-19 10:29:19 -0400

    learn about switch

---
+++ Day10/script.js
@@ -0,0 +1,69 @@
+
+// let marks = parseInt(prompt("enter your marks in % : "));
+
+
+// if(marks >= 90){
+//      console.log("you get grade A");
+
+// }else if(marks >= 80){
+//      console.log("you get gread B");
+
+// }else if(marks >= 70){
+//      console.log("you get grade C");
+
+// }else{
+//      console.log("you get grade D");
+// }
+
+
+// let ROLE = "admin";
+
+// if (ROLE === "admin") {
+
+//     console.log("you are admin")
+
+// } else if (ROLE == "user") {
+//     console.log("you are user")
+// } else {
+//     console.log("please login to get your role")
+// }
+
+
+
+let day = "sunday"
+
+switch (day) {
+
+    case "monday":
+        console.log("today is monday");
+         break;
+
+    case "tuesday":
+
+        console.log("today is tuesday");
+         break;
+
+    case "wednesday":
+
+        console.log("today is wednesday");
+        break;
+
+    case "thursday":
+
+        console.log("today is thursday");
+        break;
+
+    case "friday":
+
+        console.log("today is friday");
+        break;
+    
+    default : 
+      console.log("i dont have these day ")
+
+}
+
+
+
+
+
