d53f9b1e85885ad33cfe1a1be6f039ebd33887dd
sarrthak008 <sarthaknavale08@gmail.com>
2025-05-05 17:44:42 +0300

    learn about array and its methods

---
+++ Day11[ARRAY]/script.js
@@ -0,0 +1,105 @@
+
+// let cars = ['thar','bugati','swift','nexon']
+
+// console.log(cars[0])
+// console.log(cars[1])
+// console.log(cars[2])
+
+
+// let todos = ["wake up "] ;
+
+// todos.push("start laptop");
+// todos.push("brush")
+// todos.push("hello")
+
+// console.log(todos)
+
+
+
+// let colors = []
+
+// colors.push("red")
+// colors.push("orange")
+// colors.push("white")
+
+// console.log(colors)
+
+
+// let numbers =[]
+
+// numbers.push(100)
+// numbers.push(200)
+// numbers.push(300)
+
+// console.log(numbers)
+
+// numbers.unshift("50")
+// numbers.unshift("10")
+// numbers.unshift(0)
+
+// console.log(numbers)
+
+
+
+// let cities = []
+
+// cities.push("Pune")
+// cities.push("Mumbai")
+// cities.push("Delhi")
+// cities.unshift("satara")
+
+// console.log(cities)
+
+
+// let myName =["S","A","R","T","H","A","K"];
+
+// console.log(myName)
+
+// let removedelm =myName.pop()
+
+// console.log(removedelm)
+
+// console.log(myName)
+
+
+
+// let grade = ["A","B","C","D","E","F"]
+
+// console.log(grade)
+
+// grade.pop()
+
+// grade.push("fail")
+// grade.unshift("A+")
+
+
+// console.log(grade)
+
+// grade.shift()
+
+// console.log(grade)
+
+
+// let mobileCompanyes = ["apple"]
+
+
+// mobileCompanyes.push("samsung");
+// mobileCompanyes.push("realme")
+// mobileCompanyes.unshift("redme")
+
+// console.log(mobileCompanyes)
+
+// mobileCompanyes.shift()
+// mobileCompanyes.pop()
+
+
+// console.log(mobileCompanyes[2])
+
+
+let requests = ["sarthak"]
+
+if(requests.length <=0){
+     console.log("no one here")
+}else{
+    console.log("some reuests ")
+}
