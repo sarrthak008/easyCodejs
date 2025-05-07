8ffc84887dadae83e4290a0892ae6c44b3eced97
sarrthak008 <sarthaknavale08@gmail.com>
2025-05-07 17:13:08 +0300

    solve the doubt

--- Day13[Object proparties]/script.js
+++ Day13[Object proparties]/script.js
@@ -1,14 +1,46 @@
 
-let whatsUserData = {
+// let whatsUserData = {
      
-     name : "kiran",
+//      name : "kiran",
 
-     profile_picture : "myProfile.jpg",
+//      profile_picture : "myProfile.jpg",
 
-}
+// }
 
 
 
-//  whatsUserData.name = "sarthak"
+// //  whatsUserData.name = "sarthak"
 
-console.log(whatsUserData)
+// console.log(whatsUserData)
+
+
+
+// let PhoneInfo = {
+//      name :"iphone",
+//      prise:65000,
+//      color:"black",
+// }
+
+
+// PhoneInfo.isHaveCharger = false
+
+// delete PhoneInfo.prise
+
+// let ObjValues = Object.values(PhoneInfo)
+
+
+// console.log(ObjValues)
+
+
+// let Doubt = {
+//      doubt : "how i insert new key in object"
+// }
+
+// Doubt.isDoubtSolve =  true
+
+// Doubt.answer = "i write directly Objname.newKey = value"
+
+// Doubt.date = "12/12/1024"
+
+
+// console.log(Doubt)
