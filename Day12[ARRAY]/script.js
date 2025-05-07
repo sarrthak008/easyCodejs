a4caff4e9676c6f0cd31f7db0a23d113ae71bd67
sarrthak008 <sarthaknavale08@gmail.com>
2025-05-07 06:37:01 +0300

    day 2 of learning array

---
+++ Day12[ARRAY]/script.js
@@ -0,0 +1,26 @@
+
+// let arr= [1,2,3,4,5];
+// let abc = ['a','b','c','d','e']
+
+// arr.push(6)
+// arr.unshift(0)
+
+// arr.pop()
+
+// arr.shift()
+
+// arr.reverse()
+// abc.reverse()
+
+// console.log(abc.includes('f'))
+
+// console.log(arr.includes(4))
+
+
+// console.log(arr)
+// console.log(abc)
+
+
+let colors = ["red", "green", "blue", "yellow", "orange", "purple"];
+
+console.log(colors.indexOf("pink"));
