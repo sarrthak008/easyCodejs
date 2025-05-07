c65a195f653617367ca6912960a89c5667fb4ec9
sarrthak008 <sarthaknavale08@gmail.com>
2025-05-07 16:38:24 +0300

    push the code of the object

--- Day12[Object]/script.js
+++ Day12[Object]/script.js
@@ -1,9 +1,98 @@
 
 
-let MyInfo = {
-    name : "sarthak",
-    age : 20,
-    city: "pune"
-}
+// let MyInfo = {
+//     name : "sarthak",
+//     age : 20,
+//     city: "pune"
+// }
 
-console.log(MyInfo)
+// console.log(MyInfo)
+
+// let info = ["sarthak",20,"pune"]
+
+
+// let students = {
+
+//     student1: "Dhnashree",
+//     student2: "Divya",
+//     student3: "Kajal",
+//     student4: "Priyanka",
+//     student5: "shreya",
+//     student6: "vaishanvi"
+
+// }
+
+
+// console.log(students.student1)
+// console.log(students.student2)
+// console.log(students.student3)
+// console.log(students.student4)
+// console.log(students.student5)
+// console.log(students.student6)
+
+
+
+
+// let indianCityFoods = {
+
+//     Mumbai: ["Vada Pav", "Pav Bhaji", "Bhelpuri", "Bombay Sandwich"],
+
+//     Delhi: ["Chole Bhature", "Butter Chicken", "Parathas", "Chaat"],
+
+//     Kolkata: ["Rosogulla", "Mishti Doi", "Kathi Roll", "Phuchka"],
+
+//     Chennai: ["Idli", "Dosa", "Sambar", "Vada", "Biryani"]
+// };
+
+
+// // console.log(indianCityFoods.Mumbai[1])
+
+// // console.log(indianCityFoods.Chennai[4])
+
+// // console.log(indianCityFoods.Delhi.push("Pizza"))
+
+// // console.log(indianCityFoods.Delhi)
+
+// let info = {
+
+//      name:{
+//          first_name:"sarthak",
+//          last_name:"navale"
+//      },
+
+//      valid_user: true,
+
+//      grade :""
+// }
+
+// console.log(info.name.first_name)
+
+
+// let MyInstaData = {
+    
+//      is_private : true,
+//      accunt_name : "kajal.474",
+//      user_name : "kajal.474",
+
+//      following:{
+//           "1": "sarthak",
+//           "2" :"easyCode"
+//      },
+
+//      followers : {
+//          "1": "sarthak",
+//           "2" :"easyCode"
+//      },
+
+//      posts : [
+//          {
+//             url: "myphoto.jpg",
+//             likes : 100,
+//             date: "1/3/2024"
+//         }
+//      ]
+// }
+
+
+
+console.log(MyData.city)
