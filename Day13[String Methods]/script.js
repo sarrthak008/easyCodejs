c5f9eb42706b531e9af65e702c11375fa2d97161
sarrthak008 <sarthaknavale08@gmail.com>
2025-05-08 17:31:01 +0300

    learn basics aboutt loops

---
+++ Day13[String Methods]/script.js
@@ -0,0 +1,17 @@
+
+// let name = "easyCode"
+
+// console.log(name.toLowerCase())
+
+// let welComeMsg = "hello welcome in easyCode lectures"
+
+// console.log(welComeMsg.split(" "))
+
+// let Tmsg = "hello how are you"
+
+// console.log(Tmsg.split("T"))
+
+
+let UserName = "Dhanshree Gadhve"
+
+console.log(UserName.split("")[0])
---
+++ Day14[Loops]/script.js
@@ -0,0 +1,29 @@
+
+// let num = Number(prompt("enter the number"))
+
+// let count = 1 ;
+
+// while(count <= 10){
+//     document.writeln("<h1>" + num*count +"</h1>")
+//     count++
+// }
+
+/*
+
+10*1 =10 count++ = 1++ = 2
+
+10*2 = 20 count++  =3
+
+
+*/
+
+
+
+// let num = 10 ;
+
+// do{
+//     console.log("hello")
+//     num++
+//     console.log(num)
+
+// }while(num < 5)
