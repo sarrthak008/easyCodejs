45efef38ec393bb43c5ff5e57511abd303ea9432
sarrthak008 <sarthaknavale08@gmail.com>
2025-05-12 17:06:58 +0300

    learn about arrow functions

---
+++ Day18[functions]/app.js
@@ -0,0 +1,18 @@
+
+// let greetUser = function(name){
+//      console.log(name)
+// }
+
+// console.log(greetUser("kajal"))
+
+// node node ES-6 
+// arrow function 
+
+
+// let sayThankYou = (name) =>{
+//     console.log(`thank you ${name}`)
+// }
+
+// sayThankYou("dhanshree")
+
+
