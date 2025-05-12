4249cf791f403f67ec50a2f9c1128491c3d10536
sarrthak008 <sarthaknavale08@gmail.com>
2025-05-12 17:30:19 +0300

    learn abot IIEF callback higherorder function

--- Day18[functions]/app.js
+++ Day18[functions]/app.js
@@ -16,3 +16,64 @@
 // sayThankYou("dhanshree")
 
 
+//IIFE  Immediately invoke function expression
+
+
+// (function(){
+//     console.log("hii")
+// })()
+
+
+// call back Function:
+
+ 
+// let abc=(xyz)=>{
+//     console.log("hello")
+//     xyz()
+// }
+
+// abc(()=>{
+//     console.log("hii")
+// })
+
+// express mongo db react == callback dekhneko 
+
+
+// let greet=(cb)=>{
+//    console.log("hii")
+//    cb()
+// }
+
+// greet(()=>{
+//     console.log("greet complete")
+// })
+
+
+// Highre order function
+
+//  yesa function jo eak to parameter dusara function leta hee or return the function
+
+
+// let greet =(cb) =>{
+//     console.log("hii")
+//     cb()
+// }
+
+// greet(()=>{
+//     console.log("call back execute")
+// })
+
+// function retHelloFunction(){
+
+//     return function(){
+//         return function(){
+//              return function(){
+//                 console.log("hello")
+//              }
+//         }
+//     }
+// }
+
+// let result = retHelloFunction()
+
+// console.log(result())
